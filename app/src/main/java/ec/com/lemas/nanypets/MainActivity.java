bbc527cbc0db49f6209e3f9d3252364d53e677ca
Walther Joao <waltherjoao@gmail.com>
2022-08-02 21:52:05 -0500

    cambio del manifiesto

--- app/src/main/java/ec/com/lemas/nanypets/MainActivity.java
+++ app/src/main/java/ec/com/lemas/nanypets/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import ec.com.lemas.nanypets.menuActivitys.MiGaleria;
 import ec.com.lemas.nanypets.menuActivitys.MiVacuna;
--- app/src/main/java/ec/com/lemas/nanypets/MenuActivity.java
+++ app/src/main/java/ec/com/lemas/nanypets/MenuActivity.java
@@ -1,11 +1,10 @@
 package ec.com.lemas.nanypets;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,12 +23,13 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     ImageView ibOpinion;
     ImageView ibAgenda;
 
+    LinearLayout lytMiPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ativity_menu_principal);
-        ibPerfil  = findViewById(R.id.ib_Perfil);
+        lytMiPerfil = findViewById(R.id.lytMiPerfil);
         ibMascota= findViewById(R.id.ib_mascota);
         ibVacuna = findViewById(R.id.ib_vacuna);
         ibAlimentacion = findViewById(R.id.ib_alimentacion);
@@ -41,43 +41,20 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         ibVacuna.setOnClickListener(this);
         ibAlimentacion.setOnClickListener(this);
         ibMascota.setOnClickListener(this);
-        ibPerfil.setOnClickListener(this);
 
-
+        lytMiPerfil.setOnClickListener(this);
 
     }
+
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.ib_Perfil:
-                Intent intent = new Intent(MenuActivity.this, MiGaleria.class);
-                startActivity(intent);
+            case R.id.lytMiPerfil:
+                Intent _intent = new Intent(this, MiGaleria.class);
+                startActivity(_intent);
                 break;
-            case R.id.ib_mascota:
-                Intent intent1 = new Intent(MenuActivity.this, miMascota.class);
-                startActivity(intent1);
-                break;
-            case R.id.ib_vacuna:
-                Intent intent2 = new Intent(MenuActivity.this, MiVacuna.class);
-                startActivity(intent2);
-                break;
-            case R.id.ib_alimentacion:
-                Intent intent3 = new Intent(MenuActivity.this, miAlimentacion.class);
-                startActivity(intent3);
-                break;
-            case R.id.ib_agenda:
-                Intent intent4 = new Intent(MenuActivity.this, miAgenda.class);
-                startActivity(intent4);
-                break;
-            case R.id.ib_opinion:
-                Intent intent5 = new Intent(MenuActivity.this, miOpinion.class);
-                startActivity(intent5);
-                break;
-
-
         }
-
     }
 }
 
